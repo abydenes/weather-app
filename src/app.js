7dc2f4e48808202f0077c25ae6e40f98313bf653
abydenes <schoolreef2@protonmail.com>
2022-12-17 12:43:42 +0300

    Remove unnecessary code

--- src/app.js
+++ src/app.js
@@ -22,34 +22,3 @@ export async function getWeatherData(location = "London", units = "metric") {
     console.error(error);
   }
 }
-
-/*
-
-`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=6f72982fa697b29337f32b3e360ac876`
-
-1. Set up a blank HTML document with the appropriate links 
-to your JavaScript and CSS files.
-
-2. Write the functions that hit the API. You’re going to want 
-functions that can take a location and return the weather data 
-for that location. For now, just console.log() the information.
-
-3. Write the functions that process the JSON data you’re getting 
-from the API and return an object with only the data you require for your app.
-
-4. Set up a simple form that will let users input their location 
-and will fetch the weather info (still just console.log() it).
-
-5. Display the information on your webpage!
-
-6. Add any styling you like!
-
-7. You should be able to search for a specific location and toggle 
-displaying the data in Fahrenheit or Celsius.
-
-8. You should change the look of the page based on the data.
-
-Optional: add a ‘loading’ component that displays from the time 
-the form is submitted until the information comes back from the API.
-Push that baby to github and share your solution below!
-*/
--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ import "./style.css";
 import init from "./UI";
 
 init();
-
-// API KEY: 6f72982fa697b29337f32b3e360ac876
